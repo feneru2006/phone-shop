ffcbc7cafd166a3c36973dd4f7a174e6cce505fd
Khoa <feneru2006@gmail.com>
2026-02-28 23:03:41 +0700

    Up reportservice cho Dashboard, sua ten phone_shop trong DBConnection, up report revenuerow, report product, sua report row

--- src/DAL/DAO/DBConnection.java
+++ src/DAL/DAO/DBConnection.java
@@ -1,4 +1,4 @@
-package DAO;
+package DAL.DAO;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class DBConnection {
 
     private static final String URL =
-            "jdbc:mysql://localhost:3306/phoneshop?useSSL=false&serverTimezone=UTC";
+            "jdbc:mysql://localhost:3306/phone_shop?useSSL=false&serverTimezone=UTC";
     private static final String USER = "root";
     private static final String PASSWORD = "";
 
