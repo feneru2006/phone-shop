8e0011529ceab2c62c26dd7338dd7175e8a9b469
hungvo159 <nghiephung8277@gmail.com>
2026-03-07 21:31:31 +0700

    Update CTphieunhapBUS.java

--- src/BUS/CTphieunhapBUS.java
+++ src/BUS/CTphieunhapBUS.java
@@ -7,13 +7,9 @@ import DTO.CTphieunhapDTO;
 
 public class CTphieunhapBUS {
     private CTphieunhapDAO ctpnDAO = new CTphieunhapDAO();
-    
-    // ArrayList lưu trữ tạm thời chi tiết của tờ phiếu đang được xem
     private List<CTphieunhapDTO> listChiTietHienTai = new ArrayList<>();
 
     public CTphieunhapBUS() {}
-
-    // Lấy từ DB đổ vào ArrayList, sau đó trả ArrayList ra cho Giao diện
     public List<CTphieunhapDTO> layChiTietTheoMaPhieu(String maPNH, List<String> permissions) {
         if (permissions == null || (!permissions.contains("VIEW_NHAPHANG") && !permissions.contains("ADMIN"))) {
             System.out.println("Từ chối truy cập: Bạn không có quyền xem chi tiết phiếu nhập!");
@@ -23,4 +19,5 @@ public class CTphieunhapBUS {
         this.listChiTietHienTai = ctpnDAO.getByMaPNH(maPNH);
         return this.listChiTietHienTai;
     }
+
 }
