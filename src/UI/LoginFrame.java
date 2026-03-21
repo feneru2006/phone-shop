884fac8b465ca664f1791d4e61e90082ee531dc8
then <trinhminhthanh1106@gmail.com>
2026-03-21 20:16:16 +0700

    Update LoginFrame.java

--- src/UI/LoginFrame.java
+++ src/UI/LoginFrame.java
@@ -176,8 +176,13 @@ public class LoginFrame extends JFrame {
 
         accountDTO account = authService.login(user, pass);
         if (account != null) {
-            // Thay đổi sang giao diện chính của bạn
+            //gán dữ liệu này TRƯỚC khi khởi tạo MainFrameTest
+            Utility.SessionManager.currentUser = account;
+
+            // Sau khi đã có thông tin người dùng trong bộ nhớ, mới mở giao diện chính
             new MainFrameTest().setVisible(true);
+
+            // Đóng giao diện đăng nhập
             this.dispose();
         } else {
             JOptionPane.showMessageDialog(this, "Sai tài khoản hoặc mật khẩu!", "Lỗi đăng nhập", JOptionPane.ERROR_MESSAGE);
