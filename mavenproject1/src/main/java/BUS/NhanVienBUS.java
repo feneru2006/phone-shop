1eb92b98ac0d6eaa6f151a2383c69715125e54b0
Kiet <HIEN@LAPTOP-LAVENDER>
2026-03-24 21:03:04 +0700

    update nhân viên BUS

---
+++ mavenproject1/src/main/java/BUS/NhanVienBUS.java
@@ -0,0 +1,35 @@
+package BUS;
+
+import DAL.DAO.NhanVienDAO;
+import DTO.nhanvienDTO;
+import java.util.ArrayList;
+
+public class NhanVienBUS {
+    private NhanVienDAO nvDAO = new NhanVienDAO();
+
+    // Dùng chung cho Load Data và Tìm Kiếm
+    public ArrayList<nhanvienDTO> timKiem(String keyword) {
+        return nvDAO.timKiem(keyword);
+    }
+    
+    // Đảm bảo có hàm getList() để mấy form khác gọi (như cái PNAddDialog, GiaoDienBanHang hồi trưa)
+    public ArrayList<nhanvienDTO> getList() {
+        return nvDAO.timKiem("");
+    }
+
+    public boolean themNV(nhanvienDTO nv) {
+        return nvDAO.insert(nv);
+    }
+
+    public boolean suaNV(nhanvienDTO nv) {
+        return nvDAO.update(nv);
+    }
+
+    public boolean xoaNV(String maNV) {
+        return nvDAO.delete(maNV);
+    }
+
+    public Object layTenNhanVien(String maNV) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+}
